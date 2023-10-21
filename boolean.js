e55a1991a62ac8c6d0fa1637c0a10086acc476e1
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2023-10-21 22:41:36 +0500

    tried some operation on boolean

---
+++ boolean.js
@@ -0,0 +1,12 @@
+var foo = true;
+console.log(foo);
+
+console.log("---------------Boolean Operations--------------");
+
+// Boolean operations (&&, ||, !) work as expected:
+console.log(true && true);
+console.log(true && false);
+console.log(true || false);
+console.log(false || false);
+console.log(!true);
+console.log(!false);
