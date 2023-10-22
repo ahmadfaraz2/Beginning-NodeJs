31f0146edcbb4ee600515a395d1be23b26f720ab
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2023-10-22 12:52:26 +0500

    Immediately Executing Function in JS

---
+++ ief1.js
@@ -0,0 +1,10 @@
+// ---------------------Immediately Executing Function--------------------
+
+// We can execute a function immediately after we define it.
+// Simply wrap the function in parentheses () and invoke it.
+
+
+(function foo() {
+    console.log("foo was executed!");
+})();
+
